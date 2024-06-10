03c30e0fd144854147f86edf7aa887f6b2fb1058
Agustin Morro <agustin.morro@gmail.com>
2024-06-10 12:40:03 -0300

    ...

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -20,11 +20,11 @@ const Stats = () => {
       </section>
       <section
         id="six_section"
-        className="w-full h-screen bg-zinc-900 z-10 text-4xl sm:text-5xl  xl:text-6xl 2xl:text-8xl 2xl:leading-[5rem] font-semibold flex flex-col justify-between items-center text-white py-4 sm:py-6 xl:py-8 2xl:py-20"
+        className="w-full h-screen bg-zinc-900 text-4xl sm:text-5xl  xl:text-6xl 2xl:text-8xl 2xl:leading-[5rem] font-semibold flex flex-col justify-between items-center text-white py-4 sm:py-6 xl:py-8 2xl:py-24"
       >
-        <article className="z-50 md:self-start md:pl-[5%]">
+        <article className=" md:self-start md:pl-[5%]">
           <h6>LOREM IMPSUM </h6>
-          <h6 className="text-stone-700">LOREM IMPSUM </h6>
+          <h6 className="text-stone-700 ">LOREM IMPSUM </h6>
         </article>
         <article className="md:self-end md:pr-[4.5%]">
           <h6 className="text-stone-700">LOREM IMPSUM</h6>
