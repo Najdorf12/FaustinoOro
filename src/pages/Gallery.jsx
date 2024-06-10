c19aa13d9041ef2029cc6b00514fdea3ab32cc1c
Agustin Morro <agustin.morro@gmail.com>
2024-06-10 03:26:45 -0300

    add new section

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,7 +1,10 @@
 import imgFausti2 from "../assets/fausti2.jpg";
 import imgFausti3 from "../assets/fausti3.jpg";
 import imgFausti4 from "../assets/fausti4.jpg";
-import imgFausti5 from "../assets/fausti5.jpg";
+import imgFausti6 from "../assets/img6.jpg";
+import imgFausti7 from "../assets/img16.jpg";
+import imgFausti8 from "../assets/img18.jpg";
+import imgFausti9 from "../assets/img17.jpg";
 
 const Gallery = () => {
   return (
@@ -11,40 +14,55 @@ const Gallery = () => {
         className="w-full h-screen bg-zinc-900  font-title text-white z-50"
       >
         <article className="pt-2 px-3 flex flex-col gap-3 z-50 sm:max-w-[500px] lg:max-w-[670px]  lg:ml-16 xl:ml-32 2xl:max-w-[1100px]">
-          <h3 className="text-4xl z-50 font-medium lg:text-6xl 2xl:text-8xl">
+          <h3 className="text-5xl z-50 font-medium lg:text-6xl 2xl:text-8xl">
             LOREM IPSUM DOLOR SIT AMET CONSECT ADIPSICING ELIT.
           </h3>
         </article>
       </section>
 
-      <section id="four_section" className="w-full h-screen bg-zinc-900 overflow-hidden px-1 sm:px-4 md:px-6">
-        <div className="grid grid-cols-5 grid-rows-5 gap-2 md:gap-4 lg:gap-5 lg:ml-12 sm:max-w-[700px]  2xl:max-w-[900px] 2xl:ml-24 xl:max-h-[70%] opacity-75 ">
-          <div className="col-span-3 row-span-3 ">
+      <section
+        id="four_section"
+        className="w-full h-screen bg-zinc-900 overflow-hidden px-1 sm:px-4 md:px-6"
+      >
+        <div className="grid grid-cols-5 grid-rows-4 gap-1  h-screen opacity-80  sm:max-w-[700px]  md:gap-2 lg:gap-4 lg:h-[90%] lg:max-w-[800px] xl:max-w-[850px]  2xl:max-w-[55%] 2xl:ml-10 ">
+          <div className="col-span-3  row-span-3 ">
             <img
-              src={imgFausti2}
-              alt=""
-              className="w-full h-full object-cover rounded-md"
-            />
-          </div>
-          <div className="col-span-2 row-span-2 col-start-1 row-start-4 ">
-            <img
-              src={imgFausti3}
-              alt=""
-              className="w-full h-full object-cover rounded-md"
-            />
-          </div>
-          <div className="col-span-2 row-span-3 col-start-4 row-start-1 ">
-            <img
+              className="w-full h-full rounded-lg object-cover"
               src={imgFausti4}
               alt=""
-              className="w-full h-full object-cover rounded-md"
             />
           </div>
-          <div className="col-span-3 row-span-2 col-start-3 row-start-4 ">
+          <div className="col-span-2 row-span-2 col-start-4">
             <img
-              src={imgFausti5}
+              className="w-full h-full rounded-lg object-cover"
+              src={imgFausti2}
               alt=""
-              className="w-full h-full object-cover rounded-md"
+            />
+          </div>
+          <div className="col-span-2 row-span-2 col-start-4 row-start-3">
+            <img
+              className="w-full h-full rounded-lg object-cover "
+              src={imgFausti9}
+              alt=""
+            />
+          </div>
+          <div className="col-span-3 row-start-4  flex justify-center items-center">
+            <p className="font-title2 pl-2 sm:pl-4 text-5xl font-semibold 2xl:text-6xl text-stone-700">
+              LOREM IMPSUM
+            </p>
+          </div>
+          <div className="col-span-2 row-start-5">
+            <img
+              className="w-full h-full rounded-lg object-cover "
+              src={imgFausti6}
+              alt=""
+            />
+          </div>
+          <div className="col-span-3 col-start-3 row-start-5 ">
+            <img
+              className="w-full h-full rounded-lg object-cover"
+              src={imgFausti3}
+              alt=""
             />
           </div>
         </div>
--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,18 +1,32 @@
 const Stats = () => {
   return (
+    <>
+    
     <section
       id="five_section"
-      className="w-full h-screen bg-zinc-900 z-10 text-3xl sm:text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl 2xl:leading-[5rem] font-semibold flex flex-col justify-between items-center text-white py-4 sm:py-6 xl:py-8 2xl:py-20"
+      className="w-full h-screen bg-zinc-900 flex justify-center items-center px-2 sm:px-4 text-white"
     >
-      <article className="z-50 md:self-start md:pl-[5%]">
+    <article className="z-50 flex flex-col justify-center items-center gap-3 lg:gap-5 text-center sm:max-w-[600px] lg:max-w-[800px] xl:-mt-[10%]">
+      <h6 className="font-title text-5xl font-bold md:text-6xl lg:text-[10dvh]">LOREM IMPSUM</h6>
+      <p className="font-text text-gray-300 text-base md:text-lg lg:text-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo impedit sit aliquam laborum incidunt consectetur dicta temporibus libero, illum provident nobis, veniam sed! Voluptatem nulla rerum earum dolores alias quibusdam. Voluptatem nulla rerum earum dolores alias quibusdam.</p>
+    </article>
+    </section>
+    </>
+  );
+};
+export default Stats;
+ {/* 
+     <section
+      id="five_section"
+      className="w-full h-screen bg-zinc-700 z-10 text-4xl sm:text-5xl  xl:text-6xl 2xl:text-8xl 2xl:leading-[5rem] font-semibold flex flex-col justify-between items-center text-white py-4 sm:py-6 xl:py-8 2xl:py-20"
+    >
+     <article className="z-50 md:self-start md:pl-[5%]">
         <h6>LOREM IMPSUM </h6>
         <h6 className="text-stone-700">LOREM IMPSUM </h6>
       </article>
       <article className="md:self-end md:pr-[4.5%]">
         <h6 className="text-stone-700">LOREM IMPSUM</h6>
         <h6>LOREM IMPSUM </h6>
-      </article>
+      </article> 
     </section>
-  );
-};
-export default Stats;
+      */}
