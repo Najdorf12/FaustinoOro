0c13fec43bb4cbc7626c7978361078e96b65514d
Najdorf12 <agustin.morro@gmail.com>
2024-08-19 16:39:01 -0300

    add styles

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -71,7 +71,7 @@ const Gallery = () => {
 
       <section
         id="four_section"
-        style={{backgroundImage: "linear-gradient(to right top, #3f4864, #3d4764, #3b4664, #384664, #364564, #3b4266, #413f67, #493b67, #5f3060, #72224f, #7e1237, #80101a)"}}
+        style={{backgroundImage: "linear-gradient(to right top, #050505, #121212, #1b1a1b, #242223, #2e2a2b, #392c2e, #442e30, #4f3030, #5c2b2c, #692527, #751d21, #80101a)"}}
         className="w-full bg-gray-300 h-[90dvh] lg:h-screen z-50 2xl:z-10 overflow-y-visible relative px-2 md:pl-6 xl:pl-[4%]"
       >
         <div className="flex justify-center overflow-y-visible items-center gap-2 lg:gap-4 w-full h-[75%] max-h-screen md:w-[80%] lg:w-[70%] xl:w-[55%] ">
