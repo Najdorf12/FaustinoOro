9df0edec7c8c47fed66d689cbc48386c394bc526
Najdorf12 <agustin.morro@gmail.com>
2024-09-04 22:44:14 -0300

    add styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -64,28 +64,30 @@ const About = () => {
           scrub: true,
         },
       })
-       .to("#box-about", {
-        y:"-300px",
+      .to("#box-about", {
+        y: "-300px",
         rotate: "120deg",
         stagger: 0.06,
         scrollTrigger: {
           trigger: "#box-about",
           start: screen > 1200 ? "-120% bottom" : "-190% bottom",
-          end: screen > 1200 ? "40% 50%":"50% 60%",
+          end: screen > 1200 ? "40% 50%" : "50% 60%",
           scrub: true,
         },
-      }); 
+      });
   }, []);
   return (
-
     <>
       <section
         id="second_section"
-       /*  style={{backgroundImage: "linear-gradient(to left, #EAEAEA, #DBDBDB, #F2F2F2, #ADA996)" }}  */ 
-       style={{backgroundImage: "linear-gradient(to left top, #EAEAEA, #DBDBDB, #F2F2F2, #3f4864)" }}  
+        /*  style={{backgroundImage: "linear-gradient(to left, #EAEAEA, #DBDBDB, #F2F2F2, #ADA996)" }}  */
+        style={{
+          backgroundImage:
+            "linear-gradient(to left top, #EAEAEA, #DBDBDB, #F2F2F2, #3f4864)",
+        }}
         className="w-full h-screen bg-gray-300 flex lg:justify-end font-title overflow-hidden relative"
       >
-        <article className="relative pt-24 lg:pt-0 px-3 flex flex-col gap-3 z-50 sm:max-w-[550px] xl:mr-[7%] lg:mt-[4%] 2xl:mr-[12%] 2xl:mt-[6%]  2xl:max-w-[650px] ">
+        <article className="relative pt-24 lg:pt-0 px-3 flex flex-col gap-3 z-50 sm:max-w-[550px] xl:mr-[7%] lg:mt-[4%] 2xl:mr-[12%] 2xl:mt-[6%]  2xl:max-w-[650px] text-balance">
           <h5
             id="about-text"
             className="text-xl font-title4 text-stone-700 2xl:text-3xl opacity-0 "
@@ -94,47 +96,30 @@ const About = () => {
           </h5>
           <h3
             id="about-text2"
-            className="text-white  text-4xl 2xl:pr-2  2xl:text-5xl opacity-0 relative"
+            className="text-stone-700  text-4xl 2xl:pr-2  2xl:text-4xl  opacity-0 relative"
           >
-            ERASING NATIVE APIS LIKE INTERS OBSERV, CSS STICKY Y NOERS.
+            {"Soy Faustino Oro. Nací en Buenos Aires, Argentina, el 14 de octubre de 2013".toUpperCase()}
           </h3>
           <div
             id="line-about"
-            className="w-[0%] h-[2px]  bg-[#3f4864] rounded-md my-2"
+            className="w-[0%] h-[2px]  bg-lightbrown rounded-md my-2"
           ></div>
           <p
             id="p-about"
-            className="relative  mt-[60px] opacity-0 text-lg font-text2 xl:mt-[70px] 2xl:mt-[80px] 2xl:text-[1.5rem]  text-stone-700"
+            className="relative  mt-[60px] opacity-0 text-lg font-text2 xl:mt-[70px] 2xl:mt-[80px] 2xl:text-[1.5rem]  text-stone-900 lg:text-stone-500"
           >
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut culpa
-            consectetur ullam hic porro nobis exercitationem quam alias
-            voluptatem quasi expedita magni. Atque obcaecati aliquid praesentium
-            blanditiis, corporis necessitatibus deserunt.
+             El 30 de mayo de 2020 aprendí a mover las piezas por primera vez, y desde entonces, el ajedrez ha sido mucho más que un simple juego para mí: es un desafío constante que me motiva a seguir mejorando. Autodidacta y persistente, he crecido con cada partida y cada lección, con la firme convicción de llegar a lo más alto.
           </p>
 
-          <div className="flex gap-8 xl:gap-10 2xl:gap-12">
-            <div
-              id="btn-about"
-              className="scene opacity-0 font-text2 mt-[21px] font-medium xl:mt-5 2xl:mt-6"
-            >
-              <div className="cube">
-                <span className="side top bg- text-white">HISTORIA</span>
-                <span className="side front bg-[#3f4864] text-white">
-                  HISTORIA
-                </span>
-              </div>
-            </div>
-            <div
-              id="btn-about"
-              className="scene opacity-0 font-text2 mt-[21px] font-medium xl:mt-5 2xl:mt-6"
-            >
-              <div className="cube">
-                <span className="side top bg-[#3f4864] text-white">LOGROS</span>
-                <span className="side front  text-white font-medium">
-                  LOGROS
-                </span>
-              </div>
-            </div>
+          <div className="flex  items-center gap-6 font-title2 text-lg font-semibold text-stone-500 2xl:gap-12 2xl:text-xl">
+            <button className="btn-about border-[2px] rounded-3xl border-white px-4 py-[2.5px] 2xl:py-[2.5px] 2xl:px-6 flex justify-center items-center gap-2 hover:scale-110 duration-500 min-w-[130px]">
+              HISTORIA
+              <i class='bx bx-arrow-back text-white rotate-[145deg] text-2xl 2xl:text-3xl'></i>
+            </button>
+            <button className="btn-about border-[2px] rounded-3xl border-white px-4 py-[2.5px] 2xl:py-[2.5px] 2xl:px-7 flex justify-center items-center gap-2 hover:scale-110 duration-500 min-w-[130px]">
+              LOGROS
+              <i class='bx bx-arrow-back rotate-[145deg] text-2xl 2xl:text-3xl text-white'></i>
+            </button>
           </div>
         </article>
         <section
@@ -146,12 +131,18 @@ const About = () => {
               id="box-about"
               className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-[1px] border-white "
             ></div>
-            <div id="box-about" className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150] bg-[#3f4864]"></div>
+            <div
+              id="box-about"
+              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150] bg-[#3f4864] opacity-90 md:opacity-1"
+            ></div>
             <div
               id="box-about"
               className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-white border-[1px]"
             ></div>
-            <div id="box-about" className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864]"></div>
+            <div
+              id="box-about"
+              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864] opacity-90 md:opacity-1"
+            ></div>
             <div
               id="box-about"
               className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-white border-[1px]"
@@ -162,22 +153,17 @@ const About = () => {
               id="box-about"
               className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-[1px] border-white "
             ></div>
-            <div id="box-about" className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864]"></div>
+            <div
+              id="box-about"
+              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864] opacity-90 md:opacity-1"
+            ></div>
             <div
               id="box-about"
               className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-white border-[1px]"
             ></div>
-            <div id="box-about" className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864]"></div>
-          </div>
-          <div className="self-end z-50 ">
             <div
               id="box-about"
-              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-[1px] border-white "
-            ></div>
-            <div id="box-about" className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864]"></div>
-            <div
-              id="box-about"
-              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-[1px] border-white "
+              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864] opacity-90 md:opacity-1"
             ></div>
           </div>
           <div className="self-end z-50 ">
@@ -185,7 +171,24 @@ const About = () => {
               id="box-about"
               className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-[1px] border-white "
             ></div>
-            <div id="box-about" className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864]"></div>
+            <div
+              id="box-about"
+              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864] opacity-90 md:opacity-1"
+            ></div>
+            <div
+              id="box-about"
+              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-[1px] border-white "
+            ></div>
+          </div>
+          <div className="self-end z-50 ">
+            <div
+              id="box-about"
+              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] border-[1px] border-white "
+            ></div>
+            <div
+              id="box-about"
+              className="w-[100px] h-[100px] xl:w-[120px] 2xl:w-[150px] xl:h-[120px] 2xl:h-[150px] bg-[#3f4864] opacity-90 md:opacity-1"
+            ></div>
           </div>
           <div className="self-end z-50 ">
             <div
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
           <article className="w-[95%] flex justify-center items-end pb-[20%] xl:justify-center xl:pb-[5%] xl:">
             <h1
               id="title-name"
-              className="font-text2 tracking-wider text-center  text-[3rem] leading-[4.2rem] font-bold text-gray-200 z-50 flex  justify-center items-start md:items-center gap-3 px-3 py-3  bg-clip-text text-transparent bg-gradient-to-br from-[#ADA996] via-[#F2F2F2] to-[#DBDBDB]  md:text-[4.5rem] xl:min-h-[200px] xl:gap-8 2xl:gap-12 xl:text-[6rem] 2xl:text-[7.5rem]"
+              className="font-text2 tracking-wider text-center  text-[3rem] leading-[4.2rem] font-bold text-gray-200 z-50 flex  justify-center items-start md:items-center gap-3 px-3 py-3  bg-clip-text text-transparent bg-gradient-to-br from-[#ADA996] via-[#F2F2F2] to-stone-700  md:text-[4.5rem] xl:min-h-[200px] xl:gap-8 2xl:gap-12 xl:text-[6rem] 2xl:text-[7.5rem]"
             >
               <span className="flex justify-center items-center text-[3.9rem] h-[65px] px-1 bg-[#7c2c2c] text-white font-title2 font-semibold rounded-lg tracking-wide md:text-[4.5rem] xl:text-[8rem] xl:px-2  xl:h-[120px] xl:-mt-3 2xl:text-[9rem] 2xl:h-[150px]">
                 IM
@@ -76,22 +76,22 @@ const Home = () => {
               </section>
             ) : (
               <ul className="flex  gap-3 sm:gap-14 xl:gap-9 text-lg 2xl:text-2xl 2xl:gap-10 text-gray-200">
-                <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-5 py-[2.5px] xl:px-7 2xl:py-[2.7px] 2xl:px-6 cursor-pointer flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
+                <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-5 py-[1px] xl:px-7 2xl:py-[2.7px] 2xl:px-6 cursor-pointer flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
                   Torneos
                 </li>
                 <Link /* to="/games" */>
-                  <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-6 py-[2.5px] 2xl:py-[2.7px] 2xl:px-6 flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
+                  <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-6 py-[1px] 2xl:py-[2.7px] 2xl:px-6 flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
                     Partidas
                   </li>
                 </Link>
-                <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-6 py-[2.5px] 2xl:py-[2.7px] 2xl:px-6 flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
+                <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-6 py-[1px] 2xl:py-[2.7px] 2xl:px-6 flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
                   Noticias
                 </li>
-                <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-6 py-[2.5px] 2xl:py-[2.7px] 2xl:px-6 flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
+                <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-6 py-[1px] 2xl:py-[2.7px] 2xl:px-6 flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
                   Cursos
                 </li>
 
-                <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-6 py-[2.5px] 2xl:py-[2.7px] 2xl:px-6 flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
+                <li className="border-l-[2px] border-r-[2px] rounded-lg border-gray-400 px-6 py-[2px] 2xl:py-[2.7px] 2xl:px-6 flex justify-center items-center hover:scale-110 hover:border-red-600 duration-500">
                   Contacto
                 </li>
               </ul>
