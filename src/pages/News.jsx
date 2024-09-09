b600f1070450384d1b129e9b80b0794ba82d0f9f
Najdorf12 <agustin.morro@gmail.com>
2024-09-09 03:44:45 -0300

    add styles

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,9 +1,9 @@
 const News = () => {
     return (
         <section className="pattern w-full h-screen flex justify-center items-center">
-            <section id="news-box" className="w-[200px]
+            <section id="" className="w-[200px]
             h-[200px] xl:h-[500px] xl:w-[500px] rounded-3xl flex justify-center pt-3">
-                <h5 className="text-lightbrown text-5xl font-title">NOTICIAS</h5>
+                <h5 className="text-white text-4xl font-title font-semibold 2xl:text-8xl">NOTICIAS</h5>
             </section>
         </section>
     )
