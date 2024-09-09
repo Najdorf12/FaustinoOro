bb23d2144d08cf931ebb4d8e33d9158547cdcfb9
Najdorf12 <agustin.morro@gmail.com>
2024-09-09 03:41:22 -0300

    add styles

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,8 +1,11 @@
 const News = () => {
     return (
-        <div className="pattern w-full h-screen">
-        
-        </div>
+        <section className="pattern w-full h-screen flex justify-center items-center">
+            <section id="news-box" className="w-[200px]
+            h-[200px] xl:h-[500px] xl:w-[500px] rounded-3xl flex justify-center pt-3">
+                <h5 className="text-lightbrown text-5xl font-title">NOTICIAS</h5>
+            </section>
+        </section>
     )
 }
 export default News;
