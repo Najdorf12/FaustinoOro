8bf10354d19147f01b16c0533db8e3f94f1a909b
Najdorf12 <agustin.morro@gmail.com>
2024-09-10 01:58:29 -0300

    add styles

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,21 +3,22 @@ import imgNews4 from "../assets/news/imgNews2.jpg";
 import imgNews2 from "../assets/news/imgNews.jpg";
 import imgNews3 from "../assets/news/imgNews3.jpg";
 import { Link } from "react-router-dom";
+import CardNotice from "../components/CardNotice";
 
 const News = () => {
   return (
     <main className="pattern w-full min-h-screen pb-24 pt-20 relative flex flex-col items-center xl:pt-20 overflow-hidden">
       <Link to={"/"}>
-        <button className="absolute top-0 right-0 mt-2 mr-2 px-6 py-[2px] rounded-3xl border border-lightbrown  flex justify-center items-center text-white bg-stone-500 font-text2 text-lg xl:px-8 xl:mt-3 xl:mr-6 xl:text-xl 2xl:text-2xl  hover:scale-105 hover:text-stone-500 hover:bg-white duration-500">
+        <button className="shadow-lg shadow-zinc-900 absolute top-0 right-0 mt-2 mr-2 px-6 py-[2px] rounded-3xl border border-lightbrown  flex justify-center items-center text-white bg-stone-500 font-text2 text-lg xl:px-8 xl:mt-3 xl:mr-6 xl:text-xl 2xl:text-2xl  hover:scale-105 hover:text-stone-500 hover:bg-white duration-500">
           Volver
         </button>
       </Link>
-      <h3 className=" shadow-2xl shadow-zinc-900 w-[75%] md:w-[50%] xl:w-[35%] self-start rounded-r-2xl  max-w-[] flex justify-center items-center bg-white  font-title text-5xl text-lightbrown font-semibold  lg:text-6xl  2xl:text-7xl px-12 xl:px-14 2xl:px-16 py-3 border-[1px]  border-lightbrown lg:justify-start z-50">
+      <h3 className=" shadow-xl shadow-zinc-900 w-[75%] md:w-[50%] xl:w-[35%] self-start rounded-r-2xl  max-w-[] flex justify-center items-center bg-white  font-title text-5xl text-lightbrown font-semibold  lg:text-6xl  2xl:text-7xl px-12 xl:px-14 2xl:px-16 py-3 border-[1px]  border-lightbrown lg:justify-start z-50">
         NOTICIAS
       </h3>
 
       <section className="w-full flex flex-col items-center lg:flex-row lg:justify-center lg:gap-[6%] 2xl:gap-[10%] 2xl:justify-center  mt-4 xl:mt-6 2xl:mt-12">
-        <section className="mt-3 flex flex-col justify-center gap-2  pt-8 px-2 max-w-[600px] xl:max-w-[600px] 2xl:max-w-[700px]">
+        <section className="mt-3 flex flex-col justify-center gap-2  pt-8 px-2 max-w-[600px] xl:max-w-[600px] xl:gap-4 2xl:max-w-[700px]">
           <article
             id="box-news"
             className="relative flex flex-col items-center rounded-2xl  border border-lightbrown h-[300px] lg:h-[350px] text-balance px-3 bg-stone-600"
@@ -32,7 +33,7 @@ const News = () => {
               fugiat accusamus quod!
             </p>
           </article>
-          <div className="flex justify-center items-center gap-2 w-full font-text2">
+          <div className="flex justify-center items-center gap-2 w-full font-text2 xl:gap-3">
             <div className="text-stone-400 bg-zinc-800 relative w-1/2 text-base  sm:text-lg h-[150px] rounded-2xl rounded-tl-none rounded-br-3xl border border-lightbrown p-2 ">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis,
               maxime!
@@ -56,8 +57,8 @@ const News = () => {
           LOREM IMPSUM
         </h5> */}
 
-        <section className="mt-3 flex flex-col justify-center gap-2  pt-8 px-2 max-w-[600px] xl:max-w-[600px] 2xl:max-w-[700px] ">
-          <article id="box-news" className="flex justify-center gap-2 ">
+        <section className="mt-3 flex flex-col justify-center gap-2  pt-8 px-2 max-w-[600px] xl:max-w-[600px] 2xl:max-w-[700px] xl:gap-4">
+          <article id="box-news" className="flex justify-center gap-2 xl:gap-3">
             <picture className="relative w-[60%]">
               <img
                 className="w-full object-cover h-[300px] rounded-2xl rounded-br-none border border-stone-500 lg:h-[350px]"
@@ -79,7 +80,7 @@ const News = () => {
               </span> */}
             </div>
           </article>
-          <div className="flex justify-center items-center gap-2 w-full font-text2">
+          <div className="flex justify-center items-center gap-2 w-full font-text2 xl:gap-3">
             <div className="w-1/2  h-[150px] rounded-2xl border border-white">
               <picture className="w-full">
                 <img
@@ -99,14 +100,16 @@ const News = () => {
           </div>
         </section>
       </section>
-     
-     <article className="w-full flex flex-col justify-center items-center">
+
+      <article className="w-full flex flex-col justify-center items-center">
         {" "}
-        <h5  className="shadow-xl shadow-zinc-900 mt-20 w-[90%] max-w-[500px] text-lightbrown bg-stone-700 rounded-2xl self-center  py-3 flex justify-center items-center bg-from-stone-500   font-title text-5xl text-ligthbrown text-center  font-semibold  lg:text-6xl lg:mt-24 xl:mt-32 2xl:mt-40  2xl:text-7xl px-12 xl:px-14 2xl:px-16  z-50">
-          LOREM
+        <h5 className="shadow-xl shadow-zinc-900 mt-20 w-[90%] max-w-[500px] text-lightbrown bg-stone-700 rounded-2xl self-center  py-3 flex justify-center items-center bg-from-stone-500   font-title text-5xl text-ligthbrown text-center  font-semibold  lg:text-6xl lg:mt-24 xl:mt-32 2xl:mt-40  2xl:text-7xl px-12 xl:px-14 2xl:px-16  z-50">
+          LOREMIM
         </h5>
-       
-        <div id="news-box" className="shadow-xl  py-6 2xl:py-12 w-[90%] max-w-[900px] rounded-r-2xl mt-10 xl:mt-12 flex flex-col items-start  px-3 self-start lg:pl-[5%]">
+        <div
+          id="news-box"
+          className="shadow-xl  py-6 2xl:py-12 w-[90%] max-w-[900px] rounded-r-2xl mt-10 xl:mt-12 flex flex-col items-start  px-3 self-start lg:pl-[5%]"
+        >
           <h6 className="text-3xl font-text2 text-white text-balance max-w-[370px] lg:max-w-[500px] lg:text-5xl  text-start 2xl:text-6xl 2xl:max-w-[600px]">
             Lorem impsum dolor sit amet consectetur
           </h6>
@@ -120,6 +123,10 @@ const News = () => {
           </p>
         </div>
       </article>
+
+      <section className="w-full flex flex-wrap justify-center items-center px-3 mt-12">
+        <CardNotice />
+      </section>
     </main>
   );
 };
