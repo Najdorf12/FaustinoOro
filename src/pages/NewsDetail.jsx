1ce247bb7f8e1367cb598c742c72cba9929e524d
Najdorf12 <agustin.morro@gmail.com>
2024-09-11 14:46:19 -0300

    add styles

--- src/pages/NewsDetail.jsx
+++ src/pages/NewsDetail.jsx
@@ -4,7 +4,7 @@ import imgFausti from "../assets/fausti3.jpg";
 const NewsDetail = () => {
   return (
     <>
-      <section className="relative w-full min-h-screen bg-stone-500 pb-20 flex flex-col xl:flex-row xl:justify-center xl:items-center">
+      <section className="relative w-full min-h-screen bg-stone-500 pb-20 flex flex-col xl:flex-row xl:justify-center xl:items-center xl:gap-[5%]">
         <nav className="w-full absolute top-0 right-0  flex justify-end pr-4 pt-2 xl:pt-4">
           <ul className="flex items-center gap-4 text-stone-300">
             <Link to={"/"}>
@@ -20,11 +20,11 @@ const NewsDetail = () => {
           </ul>
         </nav> 
 
-        <div className="w-full mt-12  px-2 ">
+        <div className="w-full xl:w-auto mt-20 p-3 bg-stone-700  rounded-3xl shadow-xl shadow-zinc-800">
           <picture className="max-w-[350px] ">
             <img
               src={imgFausti}
-              className="rounded-2xl shadow-xl shadow-zinc-800 w-full max-w-[700px]"
+              className="rounded-2xl  w-full max-w-[700px] 2xl:max-w-[800px]"
               alt="imgFausti"
             />
           </picture>
@@ -32,21 +32,21 @@ const NewsDetail = () => {
 
         <section
           id="contact_section"
-          className="text-balance w-full font-text2 relative mt-6 pl-[5%] flex flex-col gap-3 2xl:gap-40 overflow-y-visible max-w-[650px] md:mt-12"
+          className="text-balance w-full font-text2 relative mt-6  flex flex-col justify-center items-center gap-3 px-3 overflow-y-visible max-w-[650px] md:mt-12"
         >
-          <h6 className="text-white relative font-title font-semibold z-50 text-3xl xl:text-4xl">
+          <h6 className="text-white relative font-title font-semibold z-50 text-3xl xl:text-4xl 2xl:text-5xl">
             FAUSTINO CONSIGUE OTRO HITO HISTORICO
             <div
               id="line-contact"
               className="w-[90%] h-[1.5px] absolute -bottom-3 bg-white z-50"
             ></div>
           </h6>
-          <p className="text-lg  text-start mt-3 text-stone-700">
+          <p className="text-lg  text-start mt-3 text-stone-700 xl:text-xl 2xl:text-xl">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et,
             consequatur nobis fuga aut reprehenderit ratione.
           </p>
 
-          <p className=" max-w-[500px] mt-3  pr-16 lg:pr-0 text-sm  md:text-base text-stone-300 2xl:text-lg font-title xl:mt- 2xl:mt-2 z-50 ">
+          <p className="  mt-3   lg:pr-0 text-sm  md:text-base text-stone-300 font-title xl:mt- 2xl:mt-2 z-50 ">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
             asperiores ut magni, porro possimus temporibus debitis tempore ex
             expedita provident corrupti rerum minus minima quaerat ducimus. Eos
