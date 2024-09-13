0519b8fc99158b0a91df0ae112b589b1a311a334
Najdorf12 <agustin.morro@gmail.com>
2024-09-13 19:26:05 -0300

    fix chessboard

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -29,6 +29,62 @@ const Games = () => {
       black: "Faustino Oro",
       pgn: "1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5  9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6  16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 19.e5 Qxa1+ 20.Ke2 Na6 21.Nxg7+ Kd8  22.Qf6+ Nxf6 23.Be7",
     },
+    {
+      players: "Faustino Oro vs Lorem Impsum",
+      location: "Lorem Impsum",
+      content: "Lorem Impsum",
+      white: "Lorem Impsum",
+      black: "Faustino Oro",
+      pgn: "1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5  9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6  16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 19.e5 Qxa1+ 20.Ke2 Na6 21.Nxg7+ Kd8  22.Qf6+ Nxf6 23.Be7",
+    },
+    {
+      players: "Faustino Oro vs Lorem Impsum",
+      location: "Lorem Impsum",
+      content: "Lorem Impsum",
+      white: "Lorem Impsum",
+      black: "Faustino Oro",
+      pgn: "1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5  9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6  16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 19.e5 Qxa1+ 20.Ke2 Na6 21.Nxg7+ Kd8  22.Qf6+ Nxf6 23.Be7",
+    },
+    {
+      players: "Faustino Oro vs Lorem Impsum",
+      location: "Lorem Impsum",
+      content: "Lorem Impsum",
+      white: "Lorem Impsum",
+      black: "Faustino Oro",
+      pgn: "1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5  9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6  16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 19.e5 Qxa1+ 20.Ke2 Na6 21.Nxg7+ Kd8  22.Qf6+ Nxf6 23.Be7",
+    },
+    {
+      players: "Faustino Oro vs Lorem Impsum",
+      location: "Lorem Impsum",
+      content: "Lorem Impsum",
+      white: "Lorem Impsum",
+      black: "Faustino Oro",
+      pgn: "1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5  9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6  16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 19.e5 Qxa1+ 20.Ke2 Na6 21.Nxg7+ Kd8  22.Qf6+ Nxf6 23.Be7",
+    },
+    {
+      players: "Faustino Oro vs Lorem Impsum",
+      location: "Lorem Impsum",
+      content: "Lorem Impsum",
+      white: "Lorem Impsum",
+      black: "Faustino Oro",
+      pgn: "1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5  9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6  16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 19.e5 Qxa1+ 20.Ke2 Na6 21.Nxg7+ Kd8  22.Qf6+ Nxf6 23.Be7",
+    },
+    {
+      players: "Faustino Oro vs Lorem Impsum",
+      location: "Lorem Impsum",
+      content: "Lorem Impsum",
+      white: "Lorem Impsum",
+      black: "Faustino Oro",
+      pgn: "1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5  9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6  16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 19.e5 Qxa1+ 20.Ke2 Na6 21.Nxg7+ Kd8  22.Qf6+ Nxf6 23.Be7",
+    },
+    {
+      players: "Faustino Oro vs Lorem Impsum",
+      location: "Lorem Impsum",
+      content: "Lorem Impsum",
+      white: "Lorem Impsum",
+      black: "Faustino Oro",
+      pgn: "1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5  9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6  16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 19.e5 Qxa1+ 20.Ke2 Na6 21.Nxg7+ Kd8  22.Qf6+ Nxf6 23.Be7",
+    },
   ];
 
   const [allGames, setAllGames] = useState(null);
@@ -90,23 +146,32 @@ const Games = () => {
             id="line-games"
             className="self-start w-[0%] h-[2px] bg-[#947153] mt-3 lg:mt-4 2xl:mt-4"
           ></div>
-          <p className="text-xl font-text2 text-stone-400 font-base mt-7 z-50 ">
+          <p className="text-xl font-text2 text-stone-400 font-base mt-7 z-50 xl:text-2xl ">
             {selectedGame
               ? selectedGame.players
               : allGames?.length > 0
               ? allGames[0].players
               : "No hay partidas disponibles"}
           </p>
+          <p className="text-base font-text2 text-stone-600 xl:text-lg">
+            {selectedGame
+              ? selectedGame.location
+              : allGames?.length > 0
+              ? allGames[0].location
+              : "No hay partidas disponibles"}
+          </p>
           <section className="chessboard-wrapper relative  max-w-[400px] 2xl:max-w-[500px] mt-14  ">
             <div id="chessboard-container" className="z-50 w-full relative">
-              <div className="font-text2 absolute z-50 -top-7 text-base text-stone-600 ">
+              <div className="font-text2 absolute z-50 -top-7 text-base text-stone-600 flex justify-center items-center gap-2">
+                <span className="w-4 h-4 rounded-full border border-white"></span>
                 {selectedGame
                   ? selectedGame.black
                   : allGames?.length > 0
                   ? allGames[0].black
                   : "No hay partidas disponibles"}
               </div>
-              <div className="font-text2 absolute z-50 bottom-[5.5rem] right-0 text-base text-stone-600 ">
+              <div className="font-text2 absolute z-50 bottom-[5.5rem] right-0 text-base text-stone-600 flex justify-center items-center gap-2 ">
+                <span className="w-4 h-4 rounded-full bg-white"></span>
                 {selectedGame
                   ? selectedGame.white
                   : allGames?.length > 0
@@ -121,7 +186,7 @@ const Games = () => {
                 customDarkSquareStyle={{ backgroundColor: "#8e1d22" }}
                 customLightSquareStyle={{ backgroundColor: "#947153" }}
               />
-              <div className=" flex items-center mt-6 gap-8 justify-evenly text-stone-400 relative">
+              <div className=" flex items-center mt-6 gap-8 justify-evenly text-stone-600 relative">
                 <button className="text-7xl" onClick={handleMoveBack}>
                   <i className="bx bx-chevrons-left"></i>
                   <p className="text-lg font-semibold -mt-2 text-lightbrown">
@@ -140,17 +205,18 @@ const Games = () => {
           </section>
         </div>
 
-        <section className="w-full flex flex-col  mt-12 xl:mt-20 2xl:mt-24  z-50 bg-red-600 lg:w-auto">
-          <ul className="w-[90%] z-50 text-lg font-title text-stone-500 font-normal bg-teal-600 flex flex-col justify-center items-center gap-1 lg:w-auto 2xl:text-[1.3rem] 2xl:gap-6 ">
+        <section className="w-full flex flex-col  mt-12 lg:w-auto  xl:mt-20 2xl:mt-24  z-50  ">
+          <ul className="w-[90%] z-50 text-lg font-title text-stone-300 font-normal  flex flex-col justify-center items-center gap-1 lg:gap-4 lg:w-auto 2xl:text-[1.3rem] 2xl:gap-6 ">
             {allGames?.map((partida, i) => {
               return (
                 <li
+                id="news-box"
                   key={i}
                   onClick={() => setSelectedGame(partida)}
-                  className="relative w-[80%] bg-rose-700 p-1 flex justify-start
-                 items-center text-base gap-2"
+                  className="relative w-[80%] p-1 flex justify-start
+                 items-center text-base gap-2 rounded-2xl z-50 px-3 lg:w-[430px]"
                 >
-                  <i className="bx bx-arrow-back rotate-[145deg] flex justify-center items-center rounded-full text-lightbrown h-7 w-7 text-xl bg-stone-600 "></i>
+                  <i className="bx bx-arrow-back rotate-[145deg] flex justify-center items-center rounded-full bg-zinc-800 text-lightbrown h-7 w-7 text-xl "></i>
                   {partida.players}
                 </li>
               );
