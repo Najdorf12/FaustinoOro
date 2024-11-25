d2b7025796c45adf3992437b0b5c61c45f7cb792
Najdorf12 <agustin.morro@gmail.com>
2024-11-25 17:59:22 -0300

    add styles

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
+import imgBg from "/bg/bg3.jpg";
 
 const Courses = () => {
   return (
     <>
-      <section className="pattern w-full h-screen pt-16 flex flex-col items-center xl:pt-32 2xl:pt-40">
-        <div className="text-stone-400 text-4xl font-title font-semibold  flex flex-col items-center justify-center gap-6 text-center px-3 text-balance max-w-[700px] xl:max-w-[900px] 2xl:max-w-[1000px]  xl:text-5xl 2xl:text-6xl 2xl:gap-12">
+      <section className="bg-whiteCustom w-full h-screen pt-16 flex flex-col items-center xl:pt-32 2xl:pt-40">
+        <div className="text-zinc-600 text-4xl font-title font-semibold  flex flex-col items-center justify-center gap-6 text-center px-3 text-balance max-w-[700px] xl:max-w-[900px] 2xl:max-w-[1000px]  xl:text-5xl 2xl:text-6xl 2xl:gap-9">
           ¡UPS ...!
           <p className="">NO HAY CURSOS DISPONIBLES EN ESTE MOMENTO</p>
         </div>
         <Link to={"/"}>
-          <button className="px-4 py-1 mt-12 rounded-3xl border border-lightbrown flex justify-center items-center text-white bg-stone-500 font-text2 text-lg xl:px-7 xl:text-xl 2xl:px-8 2xl:text-2xl 2xl:mt-24 hover:scale-105 hover:text-stone-700 hover:bg-stone-500 duration-500">
-            Volver al Inicio
+          <button className="animated-button rounded-3xl w-[190px] mt-6 pl-9 py-1 shadow-lg shadow-zinc-900 border border-zinc-600 lg:w-[210px] lg:mt-8 lg:rounded- 2xl:pl-14 2xl:w-[230px] group">
+            <i className="bx bx-right-arrow-alt arr-2 text-3xl  text-sky-700 2xl:text-4xl"></i>
+            <span className="text font-title2  text-base 2xl:text-lg text-zinc-700 group-hover:text-whiteCustom">
+              Volver al Inicio
+            </span>
+            <span className="circle bg-zinc-700"></span>
+            <i className="bx bx-right-arrow-alt arr-1 text-3xl  text-sky-700 2xl:text-4xl"></i>
           </button>
         </Link>
       </section>
