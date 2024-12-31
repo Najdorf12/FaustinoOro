26e4bf33071a522b400921e94ea24773bd3bfaeb
Najdorf <agustin.morro@gmail.com>
2024-12-30 23:55:20 -0300

    add styles

--- src/pages/News/NewsDetail.jsx
+++ src/pages/News/NewsDetail.jsx
@@ -22,7 +22,7 @@ const NewsDetail = () => {
       <section className="relative w-full min-h-screen bg-zinc-800 flex flex-col overflow-hidden">
         <nav className="w-full absolute top-0 z-[100] flex justify-start items-center  ">
           <Link to={"/news"} className="font-title ">
-            <button className="text-white border-b text-base font-normal border-lightbrown bg-zinc-800 rounded-br-xl px-12 py-2 z-50 xl:text-xl xl:px-14 2xl:px-16 2xl:text-2xl">
+            <button className="text-whiteCustom  text-base font-normal  bg-[#4b718a] rounded-br-xl px-12 py-2 z-50 xl:text-xl xl:px-14 2xl:px-16 2xl:text-2xl">
               Volver
             </button>
           </Link>
@@ -40,17 +40,17 @@ const NewsDetail = () => {
 
         <article
           id=""
-          className="text-balance  font-normal self-center relative flex flex-col justify-center items-center gap-3 w-[90%]  max-w-[650px] mt-6 text-start md:self-start md:gap-5 md:pl-[5%] xl:max-w-[1000px] xl:mt-12 2xl:mt-16 2xl:max-w-[900px]"
+          className="text-balance  font-normal self-center relative flex flex-col justify-center items-center gap-3 w-[90%]  max-w-[650px] mt-6 text-start md:self-start md:gap-5 md:pl-[5%] xl:max-w-[1000px] xl:mt-16 2xl:mt-16 2xl:max-w-[900px]"
         >
-          <h6 className="text-white  relative font-title4 font-medium z-50 text-2xl md:text-4xl xl:text-5xl 2xl:text-6xl self-start">
+          <h6 className="text-whiteCustom  relative font-text2 font-normal z-50 text-2xl md:text-4xl lg:text-6xl 2xl:text-6xl self-start">
             {notice?.title}
-            <div className="w-[90%] h-[1.5px] absolute left-0 -bottom-3 bg-white z-50 xl:-bottom-6"></div>
+            <div className="w-[90%] h-[1.5px] absolute left-0 -bottom-3 bg-[#4b718a] z-50 xl:-bottom-6"></div>
           </h6>
-          <p className="font-text2 text-lg  leading-5 mt-4 text-stone-300 md:text-xl xl:text-2xl 2xl:text-3xl 2xl:mt-6">
+          <p className="font-text2  text-lg  leading-5 mt-4 text-zinc-400 md:text-xl lg:text-2xl lg:mt-6 2xl:text-3xl 2xl:mt-6">
             {notice?.description}
           </p>
 
-          <p className="font-title mt-2 text-sm md:text-base text-stone-400  z-50 2xl:text-lg ">
+          <p className="font-title mt-2 text-sm md:text-base text-zinc-500  z-50  xl:text-lg ">
             {notice?.content}
           </p>
         </article>
