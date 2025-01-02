26c8fd54d4241cec16f27a960c2871f8fb856c0e
Najdorf <agustin.morro@gmail.com>
2025-01-02 13:13:28 -0300

    add styles

--- src/pages/Tournaments/CardTournamentPage.jsx
+++ src/pages/Tournaments/CardTournamentPage.jsx
@@ -6,7 +6,7 @@ const CardTournamentPage = ({ tournament }) => {
         <img className="w-[300px] object-cover object-center" src={images[0].secure_url} alt="img-tournament" />
       </figure>
       <article className="text-balance font-text2 text-zinc-500 flex flex-col items-start  mt-4">
-        <div className="text-2xl">{title}</div>
+        <div className="text-2xl text-whiteCustom bg-sky-800">{title}</div>
         <div>{description}</div>
         <div>{location}</div>
         <div>{time}</div>
