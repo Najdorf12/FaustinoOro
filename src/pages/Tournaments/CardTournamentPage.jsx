fdde88540adf4c0739696c091c0d4bab89a78a54
Najdorf <agustin.morro@gmail.com>
2025-01-03 22:56:43 -0300

    add styles

--- src/pages/Tournaments/CardTournamentPage.jsx
+++ src/pages/Tournaments/CardTournamentPage.jsx
@@ -1,21 +1,30 @@
 const CardTournamentPage = ({ tournament }) => {
   const { title, description, content, location, time, images } = tournament;
   return (
-    <li className="px-3 flex flex-col lg:flex-row lg:justify-center lg:items-center lg:gap-3 xl:gap-6">
+    <li className="px-3 flex flex-col lg:flex-row lg:justify-center lg:items-start  lg:gap-3 xl:gap-6">
       <figure className="w-full">
         <img
-          className="w-[300px] object-cover object-center"
+          className="w-[300px] object-cover object-center "
           src={images[0].secure_url}
           alt="img-tournament"
         />
       </figure>
-      <article className="text-balance font-text2 text-zinc-500 flex flex-col items-start  mt-4 text-xl  lg:max-w-[700px] xl:max-w-[900px]">
-        <div className="text-2xl text-whiteCustom bg-sky-800 py-2 pl-3 w-[80%]">
-          {title}
+      <article className="text-balance font-text2 text-zinc-500 flex flex-col items-start gap-1  mt-4 text-lg lg:text-xl  lg:mt-0 lg:gap-2  lg:max-w-[700px] xl:max-w-[900px] 2xl:max-w-[1000px]">
+        <div className="text-2xl lg:text-3xl  text-whiteCustom bg-sky-800 py-2 pl-3 w-full md:w-[80%] ">
+          {title.toUpperCase()}
         </div>
-        <div className="mt-3">{description}</div>
-        <div className="">{location}</div>
-        <div className="">{time}</div>
+        <div className="mt-2 flex justify-center items-center gap-2 lg:gap-3">
+          <i className="bx bxs-map text-zinc-600 text-2xl lg:text-3xl"></i>
+          {location}
+        </div>
+        <div className="flex justify-center items-center gap-2 lg:gap-3">
+          <i className="bx bx-calendar text-zinc-600  text-2xl lg:text-3xl"></i>
+          {time}
+        </div>
+        <div className="flex justify-center items-center gap-2 lg:gap-3">
+          <i className="bx bxs-pen text-zinc-600  text-2xl lg:text-3xl"></i>
+          {description}
+        </div>
         <div className="text-sm md:text-base mt-3 text-zinc-600">{content}</div>
       </article>
     </li>
--- src/pages/Tournaments/Tournaments.jsx
+++ src/pages/Tournaments/Tournaments.jsx
@@ -1,33 +1,41 @@
 import { useAdminData } from "../Admin/AdminDataContext";
 import CardTournamentPage from "./CardTournamentPage";
 import iconNav from "/iconNav.png";
+import { Link } from "react-router-dom";
 
 const Tournaments = () => {
   const { tournaments } = useAdminData();
 
   return (
-    <section className="w-full min-h-screen bg-zinc-800 font-text2">
+    <section className="overflow-hidden w-full min-h-screen bg-zinc-800 font-text2 pb-12 lg:pb-40">
       <nav className="w-full flex justify-between items-center pt-1  px-4 lg:pt-2 lg:px-12">
         <figure className="w-10 ">
           <img src={iconNav} alt="icon-nav" className="w-full object-center" />
         </figure>
         <ul className="flex items-center gap-6 text-whiteCustom text-base lg:text-lg">
-          <li className="border-l border-zinc-600 pl-2 lg:pl-3 ">Inicio</li>
-          <li className="border-l border-zinc-600 pl-2 lg:pl-3">Noticias</li>
+          <Link to={"/"}>
+            <li className="border-l border-zinc-600 pl-2 lg:pl-3 ">Inicio</li>
+          </Link>
+          <Link to={"/news"}>
+            <li className="border-l border-zinc-600 pl-2 lg:pl-3">Noticias</li>
+          </Link>
         </ul>
       </nav>
-      <article className="mt-7 flex flex-col px-3 text-balance xl:pl-12">
-        <h6 className="text-5xl font-title4 font-medium text-whiteCustom lg:text-6xl xl:text-7xl 2xl:text-8xl">TORNEOS</h6>
-        <p className="mt-3 text-zinc-500 lg:text-lg lg:max-w-[700px] 2xl:text-xl">
+      <article className="mt-10 flex flex-col px-3 text-balance lg:pl-20 lg:mt-12">
+        <h6 className="text-5xl font-title4 font-medium text-whiteCustom lg:text-6xl xl:text-7xl 2xl:text-8xl">
+          TORNEOS
+        </h6>
+        <div className="h-[2px] xl:w-[470px] mt-2  bg-zinc-700"></div>
+        <p className="mt-3 text-zinc-500 lg:mt-5 lg:text-lg lg:max-w-[700px] 2xl:text-xl">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
           quae, quibusdam labore itaque magnam totam iure minima harum repellat
           dolore. Officia tempore vero vitae distinctio labore excepturi culpa
           repudiandae voluptatibus?
         </p>
       </article>
-      <ul className="mt-12 flex flex-col items-center gap-y-9 lg:gap-y-16 ">
-        {tournaments?.map((tournament)=>(
-          <CardTournamentPage  tournament={tournament}/>
+      <ul className="mt-14 flex flex-col items-center gap-y-12 lg:gap-y-20  lg:mt-28 xl:gap-y-24">
+        {tournaments?.map((tournament) => (
+          <CardTournamentPage tournament={tournament} />
         ))}
       </ul>
     </section>
