991705107e7ea1e80fc56dfb69ed5446967b135c
Najdorf <agustin.morro@gmail.com>
2025-01-07 00:15:15 -0300

    add styles tournaments

--- src/pages/News/NewsDetail.jsx
+++ src/pages/News/NewsDetail.jsx
@@ -29,7 +29,7 @@ const NewsDetail = () => {
           </Link>
         </nav>
 
-        <div className="w-[95%] h-[30vh] mt-20 self-center md:h-[450px] md:max-w-[900px] md:self-end md:mr-[3%] 2xl:max-w-[1100px] 2xl:h-[530px]">
+        <div className="w-[95%] h-[30vh] mt-20 self-center md:h-[450px] md:max-w-[900px] md:self-end md:mr-[3%] lg:h-[500px] 2xl:max-w-[1100px] 2xl:h-[570px]">
           <figure className="w-full h-full">
             <img
               src={notice?.images[0].secure_url}
@@ -43,14 +43,14 @@ const NewsDetail = () => {
           id=""
           className="text-balance font-normal self-center relative flex flex-col justify-center items-start gap-3 w-[90%] max-w-[650px] mt-6 text-start md:self-start md:gap-5 md:pl-[5%] lg:max-w-[100%] lg:mt-16 2xl:mt-16"
         >
-          <h6 className="text-whiteCustom relative font-text2 font-normal z-50 text-2xl md:text-4xl lg:text-8xl max-w-[1300px] lg:text-balance  lg:leading-[5.24rem] 2xl:text-7xl self-start">
+          <h6 className="text-whiteCustom self-start relative font-text2 font-normal z-50 text-2xl md:text-4xl lg:text-8xl max-w-[1300px] lg:text-balance  lg:leading-[5.24rem] 2xl:text-[7rem] 2xl:leading-[6.5rem] 2xl:max-w-[1500px] ">
             {notice?.title.toUpperCase()} 
           </h6>
-          <p className="font-text2 text-lg leading-5 mt-4 text-zinc-400 md:text-xl max-w-[1100px] lg:text-2xl lg:mt-4 2xl:text-3xl 2xl:mt-6">
+          <p className="font-text2 text-lg leading-5 mt-4 text-zinc-400 md:text-xl max-w-[1100px] lg:text-2xl lg:mt-4 2xl:text-3xl 2xl:mt-2">
             {notice?.description}
           </p>
 
-          <p className="font-title mt-2 text-sm md:text-base text-zinc-500 z-50 lg:w-[70%] lg:text-lg 2xl:text-xl">
+          <p className="font-title mt-2 text-sm md:text-base text-zinc-500 z-50 lg:w-[70%] lg:text-lg 2xl:text-xl ">
             {notice?.content}
           </p>
         </article>
