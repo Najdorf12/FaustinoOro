3ff1a51ff1299923ed2a7956b9fc0fcfbbdfdd78
Najdorf <agustin.morro@gmail.com>
2025-05-15 09:25:56 -0300

    fix hdr of experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,21 +4,18 @@ import {
   OrbitControls,
   Environment,
   PerspectiveCamera,
-  Float,
 } from "@react-three/drei";
-import { GUI } from "dat.gui";
 
 const Experience = () => {
   return (
     <>
       <ambientLight intensity={2} />
-      <spotLight
-        position={[1.2, 20, 0]}
-        angle={2}
-        penumbra={1}
-        intensity={6}
+      <spotLight position={[1.2, 20, 0]} angle={2} penumbra={1} intensity={6} />
+      <Environment
+        background={false}
+        files="/potsdamer_platz_1k.hdr"
+        onError={(error) => console.error("Failed to load HDR:", error)}
       />
-      <Environment background={false} preset="city" /> 
       <PerspectiveCamera makeDefault position={[21, 3, 2]} />
       <OrbitControls enableZoom={false} enabled={false} />
       <group position={[16, -3, 2]} castShadow={true}>
